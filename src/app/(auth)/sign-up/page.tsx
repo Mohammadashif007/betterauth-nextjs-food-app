55a084bfdf7f5e51f317a565dcb285232a2ce3af
Mohammad Ashif <mohammadashif510@gmail.com>
2025-09-20 09:29:09 +0600

    signup form added

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import SignUpForm from "./sign-up-form";
+import { SignUpForm } from "./sign-up-form";
 
 const SignUp = () => {
     return (
-        <div>
+        <div className="flex min-h-svh justify-center items-center px-4">
             <SignUpForm></SignUpForm>
         </div>
     );
--- src/app/(auth)/sign-up/sign-up-form.tsx
+++ src/app/(auth)/sign-up/sign-up-form.tsx
@@ -1,13 +1,110 @@
 "use client";
 
-import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
 
-const SignUpForm = () => {
+export function SignUpForm() {
+    const form = useForm();
     return (
-        <div>
-            <h1>This is signup form</h1>
-        </div>
+        <Card className="w-full max-w-sm">
+            <CardHeader>
+                <CardTitle>Sign Up</CardTitle>
+                <CardDescription>
+                    Enter your information and create an account
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <Form {...form}>
+                    <form className="space-y-5">
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            type="text"
+                                            placeholder="Jon Doe"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            type="email"
+                                            placeholder="jon@doe.com"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            type="password"
+                                            placeholder="Password"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="confirmPassword"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Confirm Password</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            type="password"
+                                            placeholder="Confirm Password"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                    </form>
+                </Form>
+            </CardContent>
+            <CardFooter className="flex-col gap-2">
+                <Button type="submit" className="w-full">
+                    Sign Up
+                </Button>
+            </CardFooter>
+        </Card>
     );
-};
-
-export default SignUpForm;
+}
